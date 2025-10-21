f0199748972dda818e4564cc412bd1a234d97778
gitming2 <ghkfkd64a@gmail.com>
2025-10-21 04:30:22 +0000

    chore: Change runner

--- logging_langsmith_unique.py
+++ logging_langsmith_unique.py
@@ -25,7 +25,7 @@ client = Client()
 ####################################
 
 # --- 1. JSON 파일 로드 ---
-json_file_name = 'kmmlu_baseline_v1_summary.json'
+json_file_name = '/home/ghkfkd64a/K-MMLU-Refined/result/kmmlu_checkpoint-24_20251020_131200_summary.json'
 try:
     with open(json_file_name, 'r', encoding='utf-8') as f:
         data = json.load(f)
@@ -34,12 +34,12 @@ except FileNotFoundError:
     exit()
 
 # Run 설정
-RUNNER_INITIAL = "JH"  # 이름 이니셜
-CURRENT_DATE = "251019"
+RUNNER_INITIAL = "AM"  # 이름 이니셜
+CURRENT_DATE = "251020"
 MODEL_VERSION = data['model_id'].split('/')[-1]
 
 run_name = f"{CURRENT_DATE}-{RUNNER_INITIAL}-{MODEL_VERSION}"
-tags_for_run = ['K-MMLU-Evaluation', 'Baseline', 'Random-5Shot']
+tags_for_run = ['K-MMLU-Evaluation', 'sloth', 'peft', 'Random-5Shot']
 
 # run_name을 기반으로 UUID 생성
 run_id = uuid.uuid5(uuid.NAMESPACE_DNS, run_name)
@@ -65,14 +65,14 @@ try:
             project_name="K-MMLU-Refined",
             inputs={
                 "model_id": data['model_id'],
-                "seed": data['seed'],
+                "seed": data['experiment_config']['seed'], 
                 "evaluation_date": data['evaluation_date'],
-                "total_questions": data['total_questions']
+                "total_questions": data['summary']['total_questions']
             },
             outputs={
-                "overall_accuracy": data['overall_accuracy'],
-                "correct_answers": data['correct_answers'],
-                "category_accuracy": data['category_accuracy']
+                "overall_accuracy": data['summary']['overall_accuracy'],
+                "correct_answers": data['summary']['correct_answers'],
+                "category_accuracy": data['summary']['category_accuracy']
             },
             start_time=start_time,
             end_time=end_time,
